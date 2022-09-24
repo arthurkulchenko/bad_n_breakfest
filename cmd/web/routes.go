b7f9e647b0a273c56bd6fbb94867ab68965f7c5d
せんぼんざくらかげよし <artur.kulchenko@pm.me>
2022-09-24 22:22:31 +0300

    add image

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -15,5 +15,7 @@ func Routes(appP *config.AppConfig) http.Handler {
 	mux.Use(SessionLoad)
 	mux.Get("/", handlers.RepositoryPointer.Home)
 	mux.Get("/about", handlers.RepositoryPointer.About)
+	fileServer := http.FileServer(http.Dir("./static/"))
+	mux.Handle("/static/*", http.StripPrefix("/static", fileServer))
 	return mux
 }
