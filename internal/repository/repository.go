7b87bc575b87bfc7cf73caa7152ec345f0711299
せんぼんざくらかげよし <artur.kulchenko@pm.me>
2022-10-15 14:16:32 +0300

    WIP update hadlers to save data to db

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -1,5 +1,11 @@
 package repository
 
+import(
+	"github.com/arthurkulchenko/bed_n_breakfest/internal/models"
+)
+
 type DatabaseRepo interface {
 	AllUsers() bool
+
+	InsertReservation(res models.Reservation) error
 }
